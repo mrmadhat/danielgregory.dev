0ba2e71bdf04288fd28395abc967dafa8c74a8a1
mrmadhat <daniel@relativemarketing.co.uk>
2020-05-07 12:41:30 +0100

    Styling changes

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.scss"
+import { rhythm } from "../theme/typography"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -26,15 +27,11 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <div className="container">
         <main style={{ minHeight: 370 }}>{children}</main>
-        <footer>© {new Date().getFullYear()}</footer>
+        <footer style={{ marginTop: rhythm(2) }}>
+          © {new Date().getFullYear()}
+        </footer>
       </div>
     </>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,12 +10,13 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Welcome to Unfilter</h1>
     <p>
-      Not everything on your mind is public. But, that doesn’t mean it be part
-      of your timeline…
+      Not everything on your mind is public. But, that doesn’t mean it shouldn't
+      be part of your timeline…
     </p>
     <p>
       Unfilter is your online diary, you can choose to share a piece of you or
-      not, the decision is yours.
+      not, the decision is yours. By default entries added to Unfilter are
+      private but you can also share posts publicly, if you want.
     </p>
     <Button to="/join-waiting-list/">Join today</Button>
   </Layout>
