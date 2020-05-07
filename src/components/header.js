1571011b4f6458d2cd90741253260b1ef6292a9b
mrmadhat <daniel@relativemarketing.co.uk>
2020-05-07 12:41:02 +0100

    Remove bootstrap components and fix login link

--- src/components/header.js
+++ src/components/header.js
@@ -3,45 +3,52 @@ import PropTypes from "prop-types"
 import React from "react"
 import { rhythm } from "../theme/typography"
 import unfilterLogo from "../images/unfilter-logo.svg"
-import { Container, Row, Col } from "react-bootstrap"
 
 const HeaderCol = ({ children }) => (
-  <Col xs="auto" className="d-flex align-items-center">
-    {children}
-  </Col>
+  <div className="col-auto d-flex align-items-center">{children}</div>
 )
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <Container
-      style={{
-        maxWidth: "1000px",
-        marginTop: rhythm(1.5),
-        marginBottom: rhythm(1),
-      }}
-    >
-      <Row className="justify-content-between">
-        <HeaderCol>
-          <Link to="/">
-            <img
-              src={unfilterLogo}
-              alt="Unfilter Logo"
-              style={{ marginBottom: 0 }}
-            />
-          </Link>
-        </HeaderCol>
-        <HeaderCol>
-          <Link
-            to="/not-ready"
-            style={{ fontSize: "18px", fontStyle: "italic", color: "#7F7F7F" }}
-          >
-            Login
-          </Link>
-        </HeaderCol>
-      </Row>
-    </Container>
-  </header>
-)
+const Header = ({ siteTitle }) => {
+  const isHome = window.location.pathname === "/"
+
+  return (
+    <header>
+      <div
+        className="container"
+        style={{
+          marginTop: rhythm(1.5),
+          marginBottom: rhythm(1),
+        }}
+      >
+        <div className="row justify-content-between">
+          <HeaderCol>
+            <Link to="/">
+              <img
+                src={unfilterLogo}
+                alt="Unfilter Logo"
+                style={{ marginBottom: 0 }}
+              />
+            </Link>
+          </HeaderCol>
+          {isHome && (
+            <HeaderCol>
+              <Link
+                to="/join-waiting-list"
+                style={{
+                  fontSize: "18px",
+                  fontStyle: "italic",
+                  color: "#7F7F7F",
+                }}
+              >
+                Login
+              </Link>
+            </HeaderCol>
+          )}
+        </div>
+      </div>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
