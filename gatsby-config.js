b91e908b9f6fb1ad1ba3c555330b89c561c405ab
wearebattalion <74876176+wearebattalion@users.noreply.github.com>
2021-12-30 13:40:58 +0000

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,6 +75,30 @@ const seoPlugins = [
       lang: "en",
     },
   },
+  {
+    resolve: `gatsby-plugin-google-gtag`,
+    options: {
+      // You can add multiple tracking ids and a pageview event will be fired for all of them.
+      trackingIds: [
+        "G-2YM2ELHQR2", // Google Analytics / GA
+      ],
+      // This object gets passed directly to the gtag config command
+      // This config will be shared across all trackingIds
+      gtagConfig: {
+        anonymize_ip: true,
+        cookie_expires: 0,
+      },
+      // This object is used for configuration specific to this plugin
+      pluginConfig: {
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**"],
+      },
+    },
+  },
 ]
 
 const content = [
